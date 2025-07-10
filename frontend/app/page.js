2a4ccddb65f83efe4b5dd789f8ee7ac10f73b84e
João Vitor Farias <102336880+joaovfarias@users.noreply.github.com>
2025-07-09 21:11:49 -0300

    Better way to add images

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,19 +1,18 @@
-import Checkbox from '@mui/material/Checkbox';
+import Checkbox from "@mui/material/Checkbox";
+import Image from "next/image";
 
-const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
+const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
-      <img src="/images/gato.jpg" alt="image" />
-
+      <Image src="/images/gato.jpg" alt="image" width={500} height={300} />
       <div>
-      <Checkbox {...label} defaultChecked />
-      <Checkbox {...label} />
-      <Checkbox {...label} disabled />
-      <Checkbox {...label} disabled checked />
-    </div>
+        <Checkbox {...label} defaultChecked />
+        <Checkbox {...label} />
+        <Checkbox {...label} disabled />
+        <Checkbox {...label} disabled checked />
+      </div>
     </div>
   );
 }
