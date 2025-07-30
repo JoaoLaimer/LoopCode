efef8832e1fb9e4a7e2b9856e50f8d180e2368b8
Miguel Botelho <135281223+Bot-e-lho@users.noreply.github.com>
2025-07-30 18:55:16 -0300

    Update ExerciseResponseDto.java

--- backend/src/main/java/com/loopcode/loopcode/dtos/ExerciseResponseDto.java
+++ backend/src/main/java/com/loopcode/loopcode/dtos/ExerciseResponseDto.java
@@ -7,15 +7,18 @@ import java.util.UUID;
 import com.loopcode.loopcode.domain.exercise.TestCase;
 
 public record ExerciseResponseDto(
-    UUID id,
-    String title,
-    LanguageDto language,
-    String difficulty,
-    String description,
-    SimpleUserDto createdBy,
-    boolean verified,
-    LocalDateTime createdAt,
-    String mainCode,
-    List<TestCase> testCode
+        UUID id,
+        String title,
+        LanguageDto language,
+        String difficulty,
+        String description,
+        SimpleUserDto createdBy,
+        boolean verified,
+        LocalDateTime createdAt,
+        String mainCode,
+        List<TestCase> testCode,
+        int voteCount,
+        int userVote
 
-) {}
+) {
+}
