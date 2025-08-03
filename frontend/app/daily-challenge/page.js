8d047d104b47982808ec928e29b88e8e8160d731
João Vitor Farias <102336880+joaovfarias@users.noreply.github.com>
2025-08-03 19:50:14 -0300

    feat: daily-challenge page (exercise needs to be verified )

---
+++ frontend/app/daily-challenge/page.js
@@ -0,0 +1,50 @@
+"use client";
+import { Typography } from "@mui/material";
+import React from "react";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function DailyChallengePage() {
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+  const router = useRouter();
+
+  const [dailyChallenge, setDailyChallenge] = useState(null);
+
+  const getDailyChallenge = async () => {
+    const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+    try {
+      const response = await fetch(`${baseUrl}/daily-challenge`, {
+        method: "GET",
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getDailyChallenge();
+      if (data) {
+        setDailyChallenge(data);
+      } else {
+        setDailyChallenge(null);
+      }
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (dailyChallenge && dailyChallenge.id) {
+      router.replace(`/exercises/${dailyChallenge.id}`);
+    }
+  }, [dailyChallenge, router]);
+
+  return null;
+}
